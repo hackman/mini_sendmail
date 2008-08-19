de0864bf6bb2094ef9d04fad756d1013d04772f8
Marian Marinov <mm@yuhu.biz>
2008-08-19 17:00:06 +0300

    Update the version to 0.4.0 from 19.Aug.2008

--- version.h
+++ version.h
@@ -3,7 +3,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-// #define VERSION "msmtp/0.1.1 22.May.2008 "
-#define VERSION "msmtp/0.3.3 08.Jul.2008"
+#define VERSION "msmtp/0.3.6 19.Aug.2008"
 
 #endif /* _VERSION_H_ */
