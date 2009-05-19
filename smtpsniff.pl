b75269ac0ca1f260acc8850f3ff31f8564b4a679
Marian Marinov <mm@yuhu.biz>
2009-05-19 16:16:43 +0300

    Better handling of the SMTP protocol Changed the location of the logfile

--- smtpsniff.pl
+++ smtpsniff.pl
@@ -4,7 +4,7 @@ use warnings;
 use IO::Socket;
 use POSIX qw(setsid);
 
-my $logfile = '/home/hackman/smtpsniff';
+my $logfile = '/home/hackman/Projects/mini_sendmail/smtp.log';
 
 chdir '/' or die "cannot change to /:$!";
 open STDIN,'/dev/null' or die "cannot read stdin:$!";
@@ -30,19 +30,15 @@ while (1) {
 		print $s "220 MM ESMTP Sniffer\n";
 		while ( <$s> ) {
 			print LOG $_;
-			if ($_ =~ /^DATA/i) {
+			if ($_ =~ /^HELO.*/ || $_ =~ /^MAIL FROM:/ || $_ =~ /^RCPT TO:/ || $_ =~ /^\.\r\n$/ ) {
+				print $s "250 OK\n";
+			} elsif ($_ =~ /^DATA/i) {
 				print $s "354 End data with <CR><LF>.<CR><LF>\n";
-			} elsif ($_ =~ /^quit/i) {
+			} elsif ($_ =~ /^QUIT/i) {
 				print $s "221 Bye\n";
 				close $s;
-			} elsif ($_ =~ /^\.\r\n$/) {
-				print $s "250 OK\n";
-			} else {
-				print $s "250 OK\n";
 			}
 		}
     	close $s;
 	}
 }
-
-
